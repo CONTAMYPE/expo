2e17ed3c82d24509486ace05716d0653c224239c
Wiktor Smaga <wiktor.smaga13@gmail.com>
2025-06-23 14:17:11 +0200

    Make info synchronous

--- packages/expo-file-system/android/src/main/java/expo/modules/filesystem/next/FileSystemFile.kt
+++ packages/expo-file-system/android/src/main/java/expo/modules/filesystem/next/FileSystemFile.kt
@@ -6,7 +6,6 @@ import android.webkit.MimeTypeMap
 import expo.modules.filesystem.InfoOptions
 import expo.modules.filesystem.slashifyFilePath
 import expo.modules.interfaces.filesystem.Permission
-import expo.modules.kotlin.Promise
 import expo.modules.kotlin.apifeatures.EitherType
 import expo.modules.kotlin.typedarray.TypedArray
 import java.io.File
@@ -120,10 +119,10 @@ class FileSystemFile(file: File) : FileSystemPath(file) {
     return file.lastModified()
   }
 
-  fun getInfoAsync(options: InfoOptions?, promise: Promise) {
+  fun info(options: InfoOptions?): FileInfo {
     validateType()
     if (!file.exists()) {
-      return promise.reject(UnableToGetInfoAsyncException("file does not exists."))
+      throw UnableToGetInfoAsyncException("file does not exists.")
     }
     when {
       file.toURI().scheme == "file" -> {
@@ -139,9 +138,9 @@ class FileSystemFile(file: File) : FileSystemPath(file) {
             fileInfo.md5 = md5
           }
         }
-        promise.resolve(fileInfo)
+        return fileInfo
       }
-      else -> promise.reject(UnableToGetInfoAsyncException("file schema is not supported"))
+      else -> throw UnableToGetInfoAsyncException("file schema is not supported")
     }
   }
 }
