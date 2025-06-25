aa02b592e7dd139eb3dcf1f22044a1a1a4002ddd
Kudo Chien <kudo@expo.dev>
2025-06-26 02:19:22 +0800

    wip

--- packages/@expo/cli/src/export/embed/xcodeCompilerLogger.ts
+++ packages/@expo/cli/src/export/embed/xcodeCompilerLogger.ts
@@ -78,7 +78,9 @@ export function logMetroErrorInXcode(projectRoot: string, error: Error | string)
 }
 
 export function logInXcode(message: string) {
+  console.log('ooxx before logInXcode', message);
   Log.log(makeXcodeCompilerLog('note', message));
+  console.log('ooxx after logInXcode', message);
 }
 
 export function warnInXcode(message: string) {
