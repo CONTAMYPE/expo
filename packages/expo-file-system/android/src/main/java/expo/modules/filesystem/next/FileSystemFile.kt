236af7a721f97256fab409f0d5bf8b01a9b2e617
Wiktor Smaga <wiktor.smaga13@gmail.com>
2025-06-25 10:36:55 +0200

    Add url.scheme to exception message

--- packages/expo-file-system/android/src/main/java/expo/modules/filesystem/next/FileSystemFile.kt
+++ packages/expo-file-system/android/src/main/java/expo/modules/filesystem/next/FileSystemFile.kt
@@ -154,7 +154,7 @@ class FileSystemFile(file: File) : FileSystemPath(file) {
         }
         return fileInfo
       }
-      else -> throw UnableToGetInfoAsyncException("file schema is not supported")
+      else -> throw UnableToGetInfoAsyncException("file schema ${file.toURI().scheme} is not supported")
     }
   }
 }
